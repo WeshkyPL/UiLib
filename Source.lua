b3adf67f7c2d544c49bc5631fd749d6ad3828b2a
WeshkyPL <sxirbes44@gmail.com>
2025-02-14 01:14:26 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1209,6 +1209,16 @@ AddEle("Corner", function(parent, CornerRadius)
 	return New
 end)
 
+AddEle("Stroke", function(parent, props, ...)
+	local args = {...}
+	local New = InsertTheme(SetProps(Create("UIStroke", parent, {
+		Color = args[1] or Theme["Color Stroke"],
+		Thickness = args[2] or 1,
+		ApplyStrokeMode = "Border"
+	}), props), "Stroke")
+	return New
+end)
+	
 AddEle("Button", function(parent, props, ...)
 	local args = {...}
 	local New = InsertTheme(SetProps(Create("TextButton", parent, {
@@ -1227,14 +1237,6 @@ AddEle("Button", function(parent, props, ...)
 	if args[1] then
 		New.Activated:Connect(args[1])
 	end
-	return New
-end)
-
-AddEle("Gradient", function(parent, props, ...)
-	local args = {...}
-	local New = InsertTheme(SetProps(Create("UIGradient", parent, {
-		Color = Theme["Color Hub 1"]
-	}), props), "Gradient")
 	return New
 end)
 
